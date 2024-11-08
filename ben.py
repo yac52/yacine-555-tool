516599a938277eb181bdad7963db14cfae9bf884
yac52 <rwwryaw@gmail.com>
2024-11-08 22:05:03 +0100

    Create ben.py

---
+++ ben.py
@@ -0,0 +1,27 @@
+print("""
+                                                                                                                                                                             
+                                                                                                                                                                             
+BBBBBBBBBBBBBBBBB  EEEEEEEEEEEEEEEEEEEEENNNNNNNN        NNNNNNNN              AAA              BBBBBBBBBBBBBBBBB  BBBBBBBBBBBBBBBBB  EEEEEEEEEEEEEEEEEEEEEE  SSSSSSSSSSSSSSS 
+B::::::::::::::::B E::::::::::::::::::::N:::::::N       N::::::N             A:::A             B::::::::::::::::B B::::::::::::::::B E::::::::::::::::::::ESS:::::::::::::::S
+B::::::BBBBBB:::::BE::::::::::::::::::::N::::::::N      N::::::N            A:::::A            B::::::BBBBBB:::::BB::::::BBBBBB:::::BE::::::::::::::::::::S:::::SSSSSS::::::S
+BB:::::B     B:::::EE::::::EEEEEEEEE::::N:::::::::N     N::::::N           A:::::::A           BB:::::B     B:::::BB:::::B     B:::::EE::::::EEEEEEEEE::::S:::::S     SSSSSSS
+  B::::B     B:::::B E:::::E       EEEEEN::::::::::N    N::::::N          A:::::::::A            B::::B     B:::::B B::::B     B:::::B E:::::E       EEEEES:::::S            
+  B::::B     B:::::B E:::::E            N:::::::::::N   N::::::N         A:::::A:::::A           B::::B     B:::::B B::::B     B:::::B E:::::E            S:::::S            
+  B::::BBBBBB:::::B  E::::::EEEEEEEEEE  N:::::::N::::N  N::::::N        A:::::A A:::::A          B::::BBBBBB:::::B  B::::BBBBBB:::::B  E::::::EEEEEEEEEE   S::::SSSS         
+  B:::::::::::::BB   E:::::::::::::::E  N::::::N N::::N N::::::N       A:::::A   A:::::A         B:::::::::::::BB   B:::::::::::::BB   E:::::::::::::::E    SS::::::SSSSS    
+  B::::BBBBBB:::::B  E:::::::::::::::E  N::::::N  N::::N:::::::N      A:::::A     A:::::A        B::::BBBBBB:::::B  B::::BBBBBB:::::B  E:::::::::::::::E      SSS::::::::SS  
+  B::::B     B:::::B E::::::EEEEEEEEEE  N::::::N   N:::::::::::N     A:::::AAAAAAAAA:::::A       B::::B     B:::::B B::::B     B:::::B E::::::EEEEEEEEEE         SSSSSS::::S 
+  B::::B     B:::::B E:::::E            N::::::N    N::::::::::N    A:::::::::::::::::::::A      B::::B     B:::::B B::::B     B:::::B E:::::E                        S:::::S
+  B::::B     B:::::B E:::::E       EEEEEN::::::N     N:::::::::N   A:::::AAAAAAAAAAAAA:::::A     B::::B     B:::::B B::::B     B:::::B E:::::E       EEEEEE           S:::::S
+BB:::::BBBBBB::::::EE::::::EEEEEEEE:::::N::::::N      N::::::::N  A:::::A             A:::::A  BB:::::BBBBBB::::::BB:::::BBBBBB::::::EE::::::EEEEEEEE:::::SSSSSSS     S:::::S
+B:::::::::::::::::BE::::::::::::::::::::N::::::N       N:::::::N A:::::A               A:::::A B:::::::::::::::::BB:::::::::::::::::BE::::::::::::::::::::S::::::SSSSSS:::::S
+B::::::::::::::::B E::::::::::::::::::::N::::::N        N::::::NA:::::A                 A:::::AB::::::::::::::::B B::::::::::::::::B E::::::::::::::::::::S:::::::::::::::SS 
+BBBBBBBBBBBBBBBBB  EEEEEEEEEEEEEEEEEEEEENNNNNNNN         NNNNNNAAAAAAA                   AAAAAABBBBBBBBBBBBBBBBB  BBBBBBBBBBBBBBBBB  EEEEEEEEEEEEEEEEEEEEEESSSSSSSSSSSSSSS   
+                                                                                                                                                                             
+                                                                                                                                                                             
+                                                                                                                                                                             
+                                                                                                                                                                             
+                                                                                                                                                                             
+                                                                                                                                                                             
+                                                                                                                                                                             
+""")
